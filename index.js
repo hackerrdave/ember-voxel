41765b0243b498f8c797d13de1300974ae4b1886
Dave Kerr <davek09@gmail.com>
2016-02-03 21:45:47 -0500

    remove unnecessary pointer to blueprints location

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 /* jshint node: true */
 'use strict';
 
-var path = require('path');
-
 module.exports = {
   name: 'ember-voxel',
-  blueprintsPath: function() {
-    return path.join(__dirname, 'blueprints');
-  },
 
   included: function(app) {
     this._super.included(app);
